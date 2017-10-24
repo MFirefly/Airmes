b67586f2348382ea0a19282ec6fb802c740b3e46
MFirefly <maja.filakovic@gmail.com>
2017-10-24 15:53:23 +0200

    Improved charts style

--- airmes-fe/src/app/components/humidity-chart.ts
+++ airmes-fe/src/app/components/humidity-chart.ts
@@ -7,7 +7,7 @@ import { Measurement } from '../interfaces/Measurement';
     templateUrl: './humidity-chart.html'
 })
 
-export class HumidityChartComponent implements OnInit{
+export class HumidityChartComponent implements OnInit {
 
     humidityHistory: Measurement[];
     options: Object;
@@ -25,30 +25,99 @@ export class HumidityChartComponent implements OnInit{
         var timestamps = this.getTimestamps();
         var humidities = this.getHumidities();
         this.options = {
-            title: { text: '' },
+            colors: ['#f45b5b'],
+            chart: {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+                    stops: [
+                        [0, '#c7ad88'],
+                        [1, '#ede9ce']
+                    ]
+                },
+                style: {
+                    fontFamily: '\'Unica One\', sans-serif'
+                },
+                plotBorderColor: '#606063',
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Humidity - Living room',
+                style: {
+                    color: '#935347',
+                    fontSize: '20px'
+                }
+            },
             yAxis: {
                 title: {
-                    text: '[%]'
-                }
+                    text: '[%]',
+                    style: {
+                        color: '#935347'
+                    }
+                },
+                gridLineColor: '#707073',
+                labels: {
+                    style: {
+                        color: '#935347'
+                    }
+                },
+                lineColor: '#707073',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                tickWidth: 1
             },
             xAxis: {
                 categories: timestamps,
                 crosshair: false,
                 type: 'datetime',
                 labels: {
-                    format: '{value:%H:%M}'
+                    format: '{value:%H:%M}',
+                    style: {
+                        color: '#935347'
+                    }
+                },
+                gridLineColor: '#707073',
+                lineColor: '#707073',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                title: {
+                    text: 'Time',
+                    style: {
+                        color: '#935347'
+                    }
                 }
             },
             series: [{
                 name: 'Humidity',
                 data: humidities
-            }]
+            }],
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        color: '#B0B0B3'
+                    },
+                    marker: {
+                        lineColor: '#333'
+                    }
+                },
+                boxplot: {
+                    fillColor: '#505053'
+                },
+                candlestick: {
+                    lineColor: 'white'
+                },
+                errorbar: {
+                    color: 'white'
+                }
+            },
+            credits: {
+                enabled: false
+            }
         };
     }
 
     private getTimestamps(): string[] {
         let tempTimestampData = new Array(this.humidityHistory.length);
-        for(let i = 0; i < this.humidityHistory.length; i++) {
+        for (let i = 0; i < this.humidityHistory.length; i++) {
             tempTimestampData[i] = this.humidityHistory[i].time;
         }
         return tempTimestampData;
@@ -56,7 +125,7 @@ export class HumidityChartComponent implements OnInit{
 
     private getHumidities(): number[] {
         let tempHumidityData = new Array(this.humidityHistory.length);
-        for(let i = 0; i < this.humidityHistory.length; i++) {
+        for (let i = 0; i < this.humidityHistory.length; i++) {
             tempHumidityData[i] = this.humidityHistory[i].humidity;
         }
         return tempHumidityData;
--- airmes-fe/src/app/components/temperature-chart.ts
+++ airmes-fe/src/app/components/temperature-chart.ts
@@ -24,30 +24,99 @@ export class TemperatureChartComponent implements OnInit {
         var timestamps = this.getTimestamps();
         var temperatures = this.getTemperatures();
         this.options = {
-            title: { text: '' },
+            colors: ['#f45b5b'],
+            chart: {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+                    stops: [
+                        [0, '#c7ad88'],
+                        [1, '#ede9ce']
+                    ]
+                },
+                style: {
+                    fontFamily: '\'Unica One\', sans-serif'
+                },
+                plotBorderColor: '#606063',
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Temperature - Living room',
+                style: {
+                    color: '#935347',
+                    fontSize: '20px'
+                }
+            },
             yAxis: {
                 title: {
-                    text: '[°C]'
-                }
+                    text: '[°C]',
+                    style: {
+                        color: '#935347'
+                    }
+                },
+                gridLineColor: '#707073',
+                labels: {
+                    style: {
+                        color: '#935347'
+                    }
+                },
+                lineColor: '#707073',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                tickWidth: 1
             },
             xAxis: {
                 categories: timestamps,
                 crosshair: false,
                 type: 'datetime',
                 labels: {
-                    format: '{value:%H:%M}'
+                    format: '{value:%H:%M}',
+                    style: {
+                        color: '#935347'
+                    }
+                },
+                gridLineColor: '#707073',
+                lineColor: '#707073',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                title: {
+                    text: 'Time',
+                    style: {
+                        color: '#935347'
+                    }
                 }
             },
             series: [{
                 name: 'Temperature',
                 data: temperatures
-            }]
+            }],
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        color: '#B0B0B3'
+                    },
+                    marker: {
+                        lineColor: '#333'
+                    }
+                },
+                boxplot: {
+                    fillColor: '#505053'
+                },
+                candlestick: {
+                    lineColor: 'white'
+                },
+                errorbar: {
+                    color: 'white'
+                }
+            },
+            credits: {
+                enabled: false
+            }
         };
     }
 
     private getTimestamps(): string[] {
         let tempTimestampData = new Array(this.temperatureHistory.length);
-        for(let i = 0; i < this.temperatureHistory.length; i++) {
+        for (let i = 0; i < this.temperatureHistory.length; i++) {
             tempTimestampData[i] = this.temperatureHistory[i].time;
         }
         return tempTimestampData;
@@ -55,7 +124,7 @@ export class TemperatureChartComponent implements OnInit {
 
     private getTemperatures(): number[] {
         let tempTemperatureData = new Array(this.temperatureHistory.length);
-        for(let i = 0; i < this.temperatureHistory.length; i++) {
+        for (let i = 0; i < this.temperatureHistory.length; i++) {
             tempTemperatureData[i] = this.temperatureHistory[i].temperature;
         }
         return tempTemperatureData;
